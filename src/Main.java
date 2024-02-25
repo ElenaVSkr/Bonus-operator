67d2bb9a80ab8fceb0a788b764498b161af356c5
Elena S <prostovael@yandex.ru>
2024-02-25 21:21:40 +0400

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a = 100; // начальный счет
-        int b = 300; //сумма пополнения
-        int c = a + b; //итоговая сумма
-        int d = 100; // фиксированная единица
-        if (b >= 1000) {
-            System.out.println((b / d) + " количество бонусов");
-            System.out.println((a + b + b / d) + " итоговая сумма");
+        int check = 100; // начальный счет
+        int put = 300; //сумма пополнения
+        int total = check + put; //итоговая сумма
+        int unit = 100; // фиксированная единица
+        if (put >= 1000) {
+            System.out.println((put / unit) + " количество бонусов");
+            System.out.println((check + put + put / unit) + " итоговая сумма");
         } else {
             System.out.println("бонусов нет");
-            System.out.println((c) + " итоговая сумма");
+            System.out.println((total) + " итоговая сумма");
         }
     }
 }
